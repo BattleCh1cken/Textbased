56f381a7aee85f84ebe34c03816cb748a50973c9
Felix Hass <75806385+BattleCh1cken@users.noreply.github.com>
2022-05-16 16:56:58 +0000

    Added a weapon, armour, and player class.

--- GameFiles/Player_Custom.py
+++ GameFiles/Player_Custom.py
@@ -20,6 +20,49 @@ class normalending:
   def __init__(self):
     print("End of Start")
     print("Player:",starting.getname(self),"\n"," wins:",wins,"\n","loses:",loses)
+
+class Player:
+  def __init__(self):
+    self.name = "placeholder"
+    self.level = 42
+    self.damage = 42
+    self.health = 100
+    self.healthMax = 100
+    self.weapon = Weapon("basicSword", 22)
+  def equipWeapon(self, weapon):
+    self.weapon = weapon
+  
+  def calcDamage(self):
+    self.damage = self.weapon.power
+    print(self.damage)
+    
+    
+    
+
+class Weapon:
+  def __init__(self, name, power):
+    self.name = name
+    self.power = power
+
+class Armour:
+  def __init__(self, name, power):
+    self.name = name
+    self.power = power
+
+player1 = Player()
+print(player1.weapon.power)
+sword = Weapon("basicSword", 42)
+player1.equipWeapon(sword)
+print(player1.weapon.power)
+#We could calculate damage dealt based on the power of the weapon. We could also add stats like crit chance. Then we would then calculate final damage with the calculate damage method.
+  
+
+    
+    
+    
+    
+    
+
     
 #Starting up the game
-starting()
+#starting()
---
+++ ToDoList/todo.md
@@ -0,0 +1,7 @@
+# Todo
+
+- Make a item class
+- Make a player class
+- Make an inventory system
+- Make a loot generator
+- Make a map generator
